03212c36f9d21a01a817ecab74b25813233e4cc4
karshan <karshan.sharma@gmail.com>
2018-12-12 19:22:05 -0800

    websockets WIP

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,4 +1,23 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Network.HTTP.Types
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Network.Wai.Handler.WebSockets
+import           Network.WebSockets.Connection
+import           Protolude
+import           System.Environment (getArgs)
+
 main :: IO ()
-main = return ()
+main = do
+    Just port <- (readMaybe <=< head) <$> getArgs
+    run port (websocketsOr defaultConnectionOptions wsApp backupApp)
+    where
+        wsApp pendingConn = do
+            conn <- acceptRequest pendingConn
+            sendTextData conn ("Hello, client!" :: Text)
+
+        backupApp :: Application
+        backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
